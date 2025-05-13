4a927def4f19c5769e5f62ba70354aa0c3f22dbc
Aurelien <ademaille@gmail.fr>
2025-05-13 09:07:45 +0200

    Ajout de la boucle for pour l'ordre

--- pieces.js
+++ pieces.js
@@ -5,3 +5,13 @@ const pieces = [
     {nom: "Liquide de frein", prix: "9.60 €", catégorie: "Freinage"},
     {nom: "Balai d'essui-glace", prix: "29.60 €", catégorie: "Pas de catégorie"}
 ]
+
+for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i];
+    const sectionFiches = document.querySelector(".fiches");
+    const pieceElement = document.querySelector("article");
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    sectionFiches.appendChild(pieceElement);
+    pieceElement.appendChild(imageElement);
+}
