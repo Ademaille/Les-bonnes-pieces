3911d5403a037a730d797d5f2c521adddbf2cd98
Aurelien <ademaille@gmail.fr>
2025-05-13 08:11:19 +0200

    first commit

---
+++ pieces.js
@@ -0,0 +1,7 @@
+const pieces = [
+    {nom: "Ampoule LED", prix: "60 €", catégorie: "Optique"},
+    {nom: "Plaquettes de frein", prix: "40 €", catégorie: "Freinage"},
+    {nom: "Ampoule boite à gants", prix: "5.49 €", catégorie: "Optiques"},
+    {nom: "Liquide de frein", prix: "9.60 €", catégorie: "Freinage"},
+    {nom: "Balai d'essui-glace", prix: "29.60 €", catégorie: "Pas de catégorie"}
+]
